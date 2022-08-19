d6e351cea5ed8a4f68fb78fe81ef34dff68a190a
gocchan <master@gocchan.com>
2022-08-19 13:38:11 +0900

    6

--- src/main/java/com/gocchan/testgithub4/TestGitHub3.java
+++ src/main/java/com/gocchan/testgithub4/TestGitHub3.java
@@ -5,7 +5,3 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class TestGitHub3 extends JavaPlugin {
 
 }
-<<<<<<< HEAD
-=======
-//
->>>>>>> branch 'master' of git@github.com:Gocchan/TestGitHub4.git
